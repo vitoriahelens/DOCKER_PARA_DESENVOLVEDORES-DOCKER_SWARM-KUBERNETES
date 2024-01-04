cf4db938ba07e5d3b4c919dca968978280334d21
vitoriahelens <87037274+vivihelen04@users.noreply.github.com>
2024-01-04 17:38:33 -0300

    Update main.sh

--- DOCKER/volumes/main.sh
+++ DOCKER/volumes/main.sh
@@ -6,7 +6,7 @@ docker run -d -p 80:80 --name phpmessages_container phpmessages
 
 #VOLUME ANONIMO
 docker run -d -p 80:80 --name phpmessages_container --rm -v /data phpmessages
-docker inspect phpmessages_container
+docker volume inspect phpmessages_container
 
 #VOLUME NOMEADO
 ##Ao exluir o container e criar novamente com o mesmo comando, o volume irá persistir, inclusive para o mesmo container em outra porta
@@ -17,3 +17,15 @@ docker volume ls
 ##Executa o container, e o que for inserido no formulário será salvo no diretório passado
 docker run -d -p 80:80 --name phpmessages_container -v caminhodapasta\messages:/var/www/html/messages --rm phpmessages
 docker volume ls
+
+#CRIANDO VOLUME MANUALMENTE
+##No lugar de criar o volume no container, eu inicio o meu container a partir de um existente
+docker volume create nomedovolume
+docker run -d -p 80:80 --name phpmessages_container -v nomedovolume:/var/www/html/messages --rm phpmessages
+
+#REMOVENDO VOLUME
+docker volume rm nomedovolume
+docker volume prune
+
+#VOLUME APENAS LEITURA
+docker run -d -p 80:80 --name phpmessages_container -v nomedovolume:/var/www/html/messages:ro --rm phpmessages #flag -ro (read only)
