d54248dda8296118632836bb84193387dd52597c
vitoriahelens <87037274+vivihelen04@users.noreply.github.com>
2024-01-09 15:15:02 -0300

    docker compose up

--- DOCKER/docker_compose/main.sh
+++ DOCKER/docker_compose/main.sh
@@ -4,3 +4,7 @@ mkdir -p $DOCKER_CONFIG/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 docker compose version
+
+#EXECUTANDO O COMPOSE
+docker compose up
+localhost:8000 #Vai aparecer um wordpress instalado
