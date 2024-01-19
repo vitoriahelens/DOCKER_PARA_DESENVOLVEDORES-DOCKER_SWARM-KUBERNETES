9f4c84311eaf6a44d774fe9ca111166c7cb3c811
vitoriahelens <87037274+vivihelen04@users.noreply.github.com>
2024-01-19 16:08:18 -0300

    iniciando com kubernetes

---
+++ KUBERNETES/instalacao.sh
@@ -0,0 +1,25 @@
+##INSTALAÇÃO DO KUBERNETES
+
+##### WINDOWS #####
+#Instalação do Chocolatey
+Set-ExecutionPolicy AllSigned
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco -v
+#Instala o VirtualBox.exe
+choco install kubernetes-cli
+kubectl version
+#Instala o minikube
+choco install minikube
+minikube version
+
+##### LINUX #####
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+kubectl --version
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+sudo dpkg -i minikube_latest_amd64.deb
+minikube --version
