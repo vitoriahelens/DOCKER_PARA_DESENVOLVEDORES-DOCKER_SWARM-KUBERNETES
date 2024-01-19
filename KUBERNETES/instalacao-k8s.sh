11706c98e938fb42679848d6b85792e36c741b51
vitoriahelens <87037274+vivihelen04@users.noreply.github.com>
2024-01-19 16:33:55 -0300

    Update instalacao-k8s.sh

--- KUBERNETES/instalacao-k8s.sh
+++ KUBERNETES/instalacao-k8s.sh
@@ -20,8 +20,8 @@ curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
-kubectl --version
+kubectl version
 #Instalação minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
 sudo dpkg -i minikube_latest_amd64.deb
-minikube --version
+minikube version
